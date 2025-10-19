ba8e85158c3221f7a4b6b010edb7444a983ceb5f
carlos-ASG <124951093+carlos-ASG@users.noreply.github.com>
2025-10-19 15:54:21 -0700

    cambios para subir a render

--- buzon_quejas/settings.py
+++ buzon_quejas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,21 +80,15 @@ WSGI_APPLICATION = 'buzon_quejas.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Set default values for the environment variables if they’re not already set
-os.environ.setdefault("PGDATABASE", "buzon_db")
-os.environ.setdefault("PGUSER", "buzon_user")
-os.environ.setdefault("PGPASSWORD", "buzon_password")
-os.environ.setdefault("PGHOST", "localhost")
-os.environ.setdefault("PGPORT", "5432")
+os.environ.setdefault("DB_URL", "postgresql://buzon_user:buzon_password@localhost:5432/buzon_db")
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PGDATABASE', 'buzon_db'),
-        'USER': os.environ.get('PGUSER', 'buzon_user'),
-        'PASSWORD': os.environ.get('PGPASSWORD', 'buzon_password'),
-        'HOST': os.environ.get('PGHOST', 'localhost'),
-        'PORT': os.environ.get('PGPORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.environ.get("DB_URL"),
+        conn_max_age=600
+    )
 }
 
 
@@ -294,7 +289,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
